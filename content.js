1b85b2aebae1002c21d1314e69aa7c9eb2231a5c
oreki9 <44063419+oreki9@users.noreply.github.com>
2020-01-19 23:16:28 +0700

    add animate

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 wordPos = [];
 wordPos.push(AddInDict("selamat datang di dunia saya, perkenalkan saya pencipta dunia ini"));
-wordPos.push(AddInDict("saya ingin kamu menjalani dua cerita yang berbeda dan memilih jalan sesuai pilihanmu"));
+wordPos.push(AddInDict("saya ingin kamu menjalani dua cerita dan memilih jalan sesuai pilihanmu"));
 wordPos.push(AddInDict("jangan khawatir, kamu bebas dalam duniaku, kamu bisa berhenti kapan saja, berlaku apa saja saya tidak akan memaksamu"));
 wordPos.push(AddInDict("saya tak mempunyai tujuan, kamu yang datang kepada saya"));
 wordPos.push(AddInDict("sampai bertemu kembali"));
@@ -9,8 +9,8 @@ wordPos.push(AddInDict("KRING KRING KRING!!!!!"));
 wordPos.push(AddInDict("( hngg... berisik, ngantuk ... jam berapa ini ... jam 8.. JAM 8!! berangkat kerja!!!)"));
 //make array to string parser
 DialogEvent.push([{action:"Image",name:"bcg1",url:"Images/bgimage/BG3.png",x:0,y:0,z:0},{action:"Image",name:"img1",url:"Images/nanami.png",x:50,y:85,z:1},{action:"Image",name:"bc1",url:"Images/87646o.png",x:20,y:450,z:2},{action:"Text",name:"txt1",Text:DialogString(wordPos[0]),height:15,x:50,y:500,z:0,speed:10,color:"white"},{action:"Text",name:"txt2",Text:"Nanami : ",height:15,x:50,y:480,z:0,speed:0,color:"white"}]);
-DialogEvent.push([{action:"Delete",name:"txt1"},{action:"Text",name:"txt1",Text:DialogString(wordPos[1]),height:15,x:50,y:500,z:0,speed:10,color:"white"}]);
-DialogEvent.push([{action:"Delete",name:"txt1"},{action:"Text",name:"txt1",Text:DialogString(wordPos[2]),height:15,x:50,y:500,z:0,speed:10,color:"white"}]);
+DialogEvent.push([{action:"Delete",name:"txt1"},{action:"Text",name:"txt1",Text:DialogString(wordPos[1]),height:15,x:50,y:500,z:0,speed:10,color:"white"},{action:"Animate",name:"txt1",time:60,x:150,click:1}]);
+DialogEvent.push([{action:"Delete",name:"txt1"},{action:"Text",name:"txt1",Text:DialogString(wordPos[2]),height:15,x:50,y:500,z:0,speed:10,color:"white"},{action:"Animate",name:"func1",func:function(x){console.log(x[0]);x[0]-=1;},par:[4],time:40,click:1}]);
 DialogEvent.push([{action:"Delete",name:"txt1"},{action:"Text",name:"txt1",Text:DialogString(wordPos[3]),height:15,x:50,y:500,z:0,speed:10,color:"white"}]);
 DialogEvent.push([{action:"Delete",name:"txt1"},{action:"Text",name:"txt1",Text:DialogString(wordPos[4]),height:15,x:50,y:500,z:0,speed:10,color:"white"}]);
 DialogEvent.push([{action:"Delete",name:"txt1"},{action:"Text",name:"txt1",Text:DialogString(wordPos[5]),height:15,x:50,y:500,z:0,speed:10,color:"white"}]);
